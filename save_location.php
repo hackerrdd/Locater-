77e8ab9622397e3ba455dfe57bb100a41127a111
hackerrdd <165780376+hackerrdd@users.noreply.github.com>
2024-10-22 17:24:57 +0530

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
 
-    // Data ko data.txt mein append karo
+    // Append data to data.txt
     $file = fopen("data.txt", "a");
     fwrite($file, "Latitude: " . $latitude . ", Longitude: " . $longitude . "\n");
     fclose($file);
